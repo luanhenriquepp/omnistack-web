6d55d014fe12635de25230fb3e0931f20f839577
zxcvbnm <lhppaixaoo@gmail.com>
2019-10-04 20:50:21 -0300

    Finalização do projeto

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,40 +1,82 @@
-import React, { useEffect, useState } from 'react';
-import api from '../../services/api';
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState, useMemo } from "react";
+import api from "../../services/api";
+import socketio from "socket.io-client";
+import { Link } from "react-router-dom";
 
-import './style.css';
+import "./style.css";
 
 export default function Dasboard() {
-    const [spots, setSpots] = useState([]);
+  const [spots, setSpots] = useState([]);
+  const [requests, setRequests] = useState([]);
 
-    useEffect(() => {
-        async function loadSpots() {
-            const user_id = localStorage.getItem('user');
-            const response = await api.get('/dashboard', {
-                headers: { user_id }
-            })
-
-           setSpots(response.data);
+  const user_id = localStorage.getItem("user");
+  const socket = useMemo(
+    () =>
+      socketio("http://localhost:3333", {
+        query: {
+          user_id
         }
+      }),
+    [user_id]
+  );
 
-        loadSpots();
-    }, []);
-    return (
-        <>
-        <ul className="spot-list">
-            {spots.map(spot => (
-                <li key={ spot._id }>
-                    <header style={{backgroundImage: `url(${spot.thumbnail_url})`}} />
-                    <strong>{ spot.company }</strong>
-                    <span>{ spot.price ? `R$${spot.price}/dia` : 'GRATUITO' }</span>
-                </li>
-            ))}
-        </ul>
+  useEffect(() => {
+    socket.on("booking_request", data => {
+      setRequests([...requests, data]);
+    });
+  }, [requests, socket]);
 
-        <Link to="/new">
-            <button className="btn">
-                Cadastrar novo spot
-            </button></Link>
-        </>
-    )
+  useEffect(() => {
+    async function loadSpots() {
+      const user_id = localStorage.getItem("user");
+      const response = await api.get("/dashboard", {
+        headers: { user_id }
+      });
+
+      setSpots(response.data);
+    }
+
+    loadSpots();
+  }, []);
+
+  async function handleAccpet(id) {
+    await api.post(`/bookings/${id}/approvals`);
+
+    setRequests(requests.filter(requests => requests._id !== id));
+  }
+
+  async function handleReject(id) {
+    await api.post(`/bookings/${id}/rejections`);
+
+    setRequests(requests.filter(requests => requests._id !== id));
+  }
+
+  return (
+    <>
+      <ul className="notifications">
+        {requests.map(request => (
+          <li key={request._id}>
+            <p>
+              <strong>{request.user.email} </strong> está solicitando uma reserva em <strong>{request.spot.company} </strong> para a data: <strong>{request.date}</strong>
+            </p>
+            <button className="accept" onClick={() => handleAccpet(request._id)}>ACEITAR</button>
+            <button className="reject" onClick={() => handleReject(request._id)}>REJEITAR</button>
+          </li>
+        ))}
+      </ul>
+      <ul className="spot-list">
+        {spots.map(spot => (
+          <li key={spot._id}>
+            <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
+            <strong>{spot.company}</strong>
+            <span>{spot.price ? `R$${spot.price}/dia` : "GRATUITO"}</span>
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/new">
+        <button className="btn">Cadastrar novo spot</button>
+      </Link>
+    </>
+  );
 }
